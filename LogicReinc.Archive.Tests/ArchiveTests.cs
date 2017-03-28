9a5801558d69a9253185ef1d4ddd152d57f97b5b
Kelvin K <logicincarnate@gmail.com>
2017-03-28 10:14:22 +0200

    Init

---
+++ LogicReinc.Archive.Tests/ArchiveTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LogicReinc.Archive.Tests
+{
+    [TestClass]
+    public class ArchiveTests
+    {
+
+        public static Archive archive = new Archive("");
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            LRDocument doc = new LRDocument()
+            {
+                ID = Guid.NewGuid().ToString(),
+                Name = "Rhinos",
+                Tags = new List<string>()
+                {
+                    "Rhino",
+                    "Animal",
+                    "Africa"
+                },
+                Text = @"
+A rhinoceros (/raɪˈnɒsərəs/, meaning 'nose horn'), often abbreviated to rhino, is one of any five extant species of odd-toed ungulates in the family Rhinocerotidae, as well as any of the numerous extinct species. 
+Two of these extant species are native to Africa and three to Southern Asia.
+Members of the rhinoceros family are characterized by their large size(they are some of the largest remaining megafauna, with all of the species able to reach one tonne or more in weight); as well as by an herbivorous diet; a thick protective skin, 1.5–5 cm thick, formed from layers of collagen positioned in a lattice structure; relatively small brains for mammals this size(400–600 g); and a large horn.They generally eat leafy material, although their ability to ferment food in their hindgut allows them to subsist on more fibrous plant matter, if necessary.Unlike other perissodactyls, the two African species of rhinoceros lack teeth at the front of their mouths, relying instead on their lips to pluck food.[1]"
+            };
+
+            LRDocument doc2 = new LRDocument()
+            {
+                ID = Guid.NewGuid().ToString(),
+                Name = "Africa",
+                Tags = new List<string>()
+                {
+                    "Continent",
+                    "Poor",
+                    "Savage"
+                },
+                Text = @"
+Africa is the world's second-largest and second-most-populous continent. 
+At about 30.3 million km² (11.7 million square miles) including adjacent islands, it covers 6% of Earth's total surface area and 20.4 % of its total land area.[2] With 1.2 billion people as of 2016, it accounts for about 16% of the world's human population.[1] The continent is surrounded by the Mediterranean Sea to the north, both the Suez Canal and the Red Sea along the Sinai Peninsula to the northeast, the Indian Ocean to the southeast, and the Atlantic Ocean to the west. 
+The continent includes Madagascar and various archipelagos. It contains 54 fully recognized sovereign states (countries), nine territories and two de facto independent states with limited or no recognition.[3]"
+            };
+
+            LRDocument doc3 = new LRDocument()
+            {
+                ID = Guid.NewGuid().ToString(),
+                Name = "DEF CON",
+                Tags = new List<string>()
+                {
+                    "Security",
+                    "Information",
+                    "Technology",
+                    "Hackers"
+                },
+                Text = @"
+DEF CON (also written as DEFCON, Defcon, or DC) is one of the world's largest annual hacker conventions, held annually in Las Vegas, Nevada, with the first DEF CON taking place in June 1993. 
+Many of the attendees at DEF CON include computer security professionals, journalists, lawyers, federal government employees, security researchers, students, and hackers with a general interest in software, computer architecture, phone phreaking, hardware modification, and anything else that can be 'hacked.' 
+The event consists of several tracks of speakers about computer- and hacking-related subjects, as well as social events Wargames and contests in everything from creating the longest Wi-Fi connection and hacking computer systems to who can most effectively cool a beer in the Nevada heat."
+            };
+
+
+            archive.Add(doc);
+            archive.Add(doc2);
+            archive.Add(doc3);
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            archive.Close();
+            Thread.Sleep(1000);
+            archive.RootDirectory.Delete();
+        }
+
+        [TestMethod]
+        public void SearchWords()
+        {
+
+            List<LRDocumentResult> result = archive.Search("Africa");
+
+            foreach (LRDocumentResult r in result)
+                Console.WriteLine($"Found: {r.Name} with score {r.Score}");
+
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void SearchText()
+        {
+
+            List<LRDocumentResult> result = archive.Search("computer");
+
+            foreach (LRDocumentResult r in result)
+                Console.WriteLine($"Found: {r.Name} with score {r.Score}");
+
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void SearchGroup()
+        {
+
+            List<LRDocumentResult> result = archive.Search("computer", "Rhinos");
+
+            foreach (LRDocumentResult r in result)
+                Console.WriteLine($"Found: {r.Name} with score {r.Score}");
+
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void SearchStringGroup()
+        {
+
+            List<LRDocumentResult> result = archive.Search("computer Rhinos");
+
+            foreach (LRDocumentResult r in result)
+                Console.WriteLine($"Found: {r.Name} with score {r.Score}");
+
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void SearchTags()
+        {
+
+            List<LRDocumentResult> result = archive.SearchTags("Africa");
+
+            foreach (LRDocumentResult r in result)
+                Console.WriteLine($"Found: {r.Name} with score {r.Score}");
+
+            Assert.AreEqual(1, result.Count);
+        }
+    }
+}
---
+++ LogicReinc.Archive/Document.cs
@@ -0,0 +1,65 @@
+﻿using Lucene.Net.Documents;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicReinc.Archive
+{
+    public class LRDocumentResult : LRDocument
+    {
+        public LRDocumentResult(float score, Document doc) : base(doc)
+        {
+            Initialized = false;
+            Score = score;
+        }
+
+        public float Score { get; set; }
+    }
+
+    public class LRDocument
+    {
+        public static string[] Fields { get; } = new string[] { "ID", "Name", "Summary", "Tags", "Text" };
+
+        public bool Initialized { get; protected set; } = true;
+
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public List<string> Tags { get; set; }
+
+        public string Text { get; set; }
+
+        public LRDocument() { }
+
+        public LRDocument(Document doc)
+        {
+            Initialized = false;
+            ID = doc.GetField("ID").StringValue;
+            Name = doc.GetField("Name").StringValue;
+            Summary = doc.GetField("Summary").StringValue;
+            Tags = doc.GetFields("Tags").Select(x => x.StringValue).ToList();
+        }
+
+        public FileStream Read(Archive archive)
+        {
+            return new FileStream(Path.Combine(archive.DocumentDirectory.FullName, ID), FileMode.Open);
+        }
+
+        public Document CreateIndexDocument()
+        {
+            Document doc = new Document();
+            doc.Add(new Field("ID", this.ID, Field.Store.YES, Field.Index.NOT_ANALYZED));
+            doc.Add(new Field("Name", this.Name, Field.Store.YES, Field.Index.ANALYZED));
+            doc.Add(new Field("Summary", this.Summary, Field.Store.YES, Field.Index.ANALYZED));
+            doc.Add(new Field("Text", this.Text, Field.Store.NO, Field.Index.ANALYZED));
+
+            foreach (string tag in this.Tags)
+                doc.Add(new Field("Tags", tag, Field.Store.YES, Field.Index.ANALYZED));
+            return doc;
+        }
+    }
+}
